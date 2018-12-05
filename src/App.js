70ba65f634d709b6dc82dfcfd0e2780d7f9f6ac1
pzync <shkhrgrv@gmail.com>
2018-12-05 10:28:48 +0530

    initial commit with barebones features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TaskCard from "./components/taskCard";
+
+const today = new Date();
 
 class App extends Component {
+  state = {
+    taskList: []
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let id = Math.floor(Math.random() * 100) + 1;
+    let taskName = this.refs.task.value;
+    let project = this.refs.project.value;
+    let assignee = this.refs.assignee.value;
+    let workDate = this.refs.workdate.value;
+
+    this.setState({
+      taskList: [
+        ...this.state.taskList,
+        { id, taskName, project, assignee, workDate }
+      ]
+    });
+
+    this.refs.task.value = "";
+    this.refs.project.value = "";
+    this.refs.assignee.value = "";
+    this.refs.workdate.value = today.toISOString().substr(0, 10);
+
+    this.refs.task.focus();
+  };
+
+  componentDidMount() {
+    // this keeps the task input field in focus when the page loads
+    this.refs.task.focus();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="task-input"
+            type="text"
+            placeholder="Describe task here"
+            ref="task"
+          />
+          <div className="second-row">
+            <input
+              className="project-input"
+              type="text"
+              placeholder="For Project"
+              ref="project"
+            />
+            <input
+              className="assignee-input"
+              type="text"
+              placeholder="Assigned to"
+              ref="assignee"
+            />
+            <input
+              className="date-input"
+              type="date"
+              defaultValue={today.toISOString().substr(0, 10)}
+              ref="workdate"
+            />
+            <button className="submit-button" type="submit">
+              Add
+            </button>
+          </div>
+        </form>
+        <div className="list-area">
+          <TaskCard taskList={this.state.taskList} />
+        </div>
       </div>
     );
   }
---
+++ src/components/taskCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./taskCard.css";
+
+const TaskCard = ({ taskList }) => {
+  return (
+    <ul className="plan-list">
+      {taskList.map(task => (
+        <li key={task.id}>
+          <h5 className="task-text">{task.taskName}</h5>
+          <div className="task-details">
+            <a href={`/#${task.project}`} className="task-project">{`#${
+              task.project
+            }`}</a>
+            <a href={`/@${task.assignee}`} className="task-assignee">{`@${
+              task.assignee
+            }`}</a>
+            <p className="task-date">{task.workDate}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TaskCard;
